d90e4f625f1666b6594c46858688c1b8d0b9c471
Reza Babaei <ramyariran@gmail.com>
2023-11-07 14:59:44 +0700

    mui - popover

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import * as React from "react";
+import Popover from "@mui/material/Popover";
 import Typography from "@mui/material/Typography";
-import { useState } from "react";
+import Button from "@mui/material/Button";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = React.useState(0);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
 
   return (
     <>
@@ -30,14 +45,21 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
 
-      <Typography
-        fontWeight={"bold"}
-        variant="h1"
-        fontSize={"30px"}
-        textDecoration={"underline"}
+      <Button aria-describedby={id} variant="contained" onClick={handleClick}>
+        Open Popover
+      </Button>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
       >
-        Hello world!
-      </Typography>
+        <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
+      </Popover>
     </>
   );
 }
